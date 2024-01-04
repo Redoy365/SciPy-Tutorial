63433076db7bbb8109f914bd4012962f1578ec6b
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-04 17:19:56 +0600

    CSR Matrix

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from scipy.optimize import root
-from math import cos
+import numpy as np
+from scipy.sparse import csr_matrix
 
-def eqn(x):
-  return x + cos(x)
+arr = np.array([0, 0, 0, 0, 0, 1, 1, 0, 2])
 
-myroot = root(eqn, 0)
-
-print(myroot.x)
+print(csr_matrix(arr))
