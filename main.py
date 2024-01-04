dc2094314059d92f1aa615fca3c46024a4baab70
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-04 17:22:04 +0600

    eliminate_zeros()

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from scipy.sparse import csr_matrix
 
 arr = np.array([[0, 0, 0], [0, 0, 1], [1, 0, 2]])
 
-print(csr_matrix(arr).count_nonzero())
+mat = csr_matrix(arr)
+mat.eliminate_zeros()
+
+print(mat)
